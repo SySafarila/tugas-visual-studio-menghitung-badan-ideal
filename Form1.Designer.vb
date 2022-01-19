e26e28f4debf44439fdd70b7eab50507068a358e
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2022-01-19 08:05:04 +0700

    cek koneksi

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -27,12 +27,20 @@ Partial Class Form1
         Me.TextTinggiBadan = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.BtnCek = New System.Windows.Forms.Button()
+        Me.tbNama = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        Me.tbId = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Location = New System.Drawing.Point(18, 104)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(88, 13)
         Me.Label1.TabIndex = 0
@@ -40,14 +48,14 @@ Partial Class Form1
         '
         'TextBeratBadan
         '
-        Me.TextBeratBadan.Location = New System.Drawing.Point(16, 30)
+        Me.TextBeratBadan.Location = New System.Drawing.Point(21, 121)
         Me.TextBeratBadan.Name = "TextBeratBadan"
         Me.TextBeratBadan.Size = New System.Drawing.Size(100, 20)
         Me.TextBeratBadan.TabIndex = 1
         '
         'TextTinggiBadan
         '
-        Me.TextTinggiBadan.Location = New System.Drawing.Point(16, 77)
+        Me.TextTinggiBadan.Location = New System.Drawing.Point(21, 168)
         Me.TextTinggiBadan.Name = "TextTinggiBadan"
         Me.TextTinggiBadan.Size = New System.Drawing.Size(100, 20)
         Me.TextTinggiBadan.TabIndex = 3
@@ -55,7 +63,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(13, 60)
+        Me.Label2.Location = New System.Drawing.Point(18, 151)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(94, 13)
         Me.Label2.TabIndex = 2
@@ -63,18 +71,83 @@ Partial Class Form1
         '
         'BtnCek
         '
-        Me.BtnCek.Location = New System.Drawing.Point(16, 104)
+        Me.BtnCek.Location = New System.Drawing.Point(21, 195)
         Me.BtnCek.Name = "BtnCek"
         Me.BtnCek.Size = New System.Drawing.Size(100, 23)
         Me.BtnCek.TabIndex = 4
         Me.BtnCek.Text = "Cek"
         Me.BtnCek.UseVisualStyleBackColor = True
         '
+        'tbNama
+        '
+        Me.tbNama.Location = New System.Drawing.Point(21, 28)
+        Me.tbNama.Name = "tbNama"
+        Me.tbNama.Size = New System.Drawing.Size(100, 20)
+        Me.tbNama.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(18, 11)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Nama"
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Location = New System.Drawing.Point(145, 195)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(100, 23)
+        Me.btnEdit.TabIndex = 7
+        Me.btnEdit.Text = "Edit"
+        Me.btnEdit.UseVisualStyleBackColor = True
+        '
+        'btnHapus
+        '
+        Me.btnHapus.Location = New System.Drawing.Point(283, 195)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(100, 23)
+        Me.btnHapus.TabIndex = 8
+        Me.btnHapus.Text = "Hapus"
+        Me.btnHapus.UseVisualStyleBackColor = True
+        '
+        'tbId
+        '
+        Me.tbId.Location = New System.Drawing.Point(21, 73)
+        Me.tbId.Name = "tbId"
+        Me.tbId.Size = New System.Drawing.Size(100, 20)
+        Me.tbId.TabIndex = 10
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(18, 56)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(16, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Id"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 236)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(776, 202)
+        Me.DataGridView1.TabIndex = 11
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.tbId)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnHapus)
+        Me.Controls.Add(Me.btnEdit)
+        Me.Controls.Add(Me.tbNama)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.BtnCek)
         Me.Controls.Add(Me.TextTinggiBadan)
         Me.Controls.Add(Me.Label2)
@@ -82,6 +155,7 @@ Partial Class Form1
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -92,4 +166,11 @@ Partial Class Form1
     Friend WithEvents TextTinggiBadan As TextBox
     Friend WithEvents Label2 As Label
     Friend WithEvents BtnCek As Button
+    Friend WithEvents tbNama As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnEdit As Button
+    Friend WithEvents btnHapus As Button
+    Friend WithEvents tbId As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents DataGridView1 As DataGridView
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,4 +1,11 @@
-﻿Public Class Form1
+﻿Imports System.Data.Odbc
+Public Class Form1
+    Dim Conn As OdbcConnection
+    Dim Cmd As OdbcCommand
+    Dim Ds As DataSet
+    Dim Da As OdbcDataAdapter
+    Dim Rd As OdbcDataReader
+    Dim MyDB As String
     Private Sub BtnCek_Click(sender As Object, e As EventArgs) Handles BtnCek.Click
         Dim bb As Single
         Dim tb As Single
@@ -17,5 +24,19 @@
                 MessageBox.Show("Anda ideal, gooood")
             End If
         End If
+    End Sub
+
+    Sub Koneksi()
+        ' Memanggil database yaitu nama database kita adalah kampus
+        MyDB = "Driver={MySQL ODBC 3.51 Driver};Database=beratbadan;Server=localhost;uid=root"
+        Conn = New OdbcConnection(MyDB)
+        If Conn.State = ConnectionState.Closed Then
+            Conn.Open()
+            MsgBox("Koneksi berhasil")
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call Koneksi()
     End Sub
 End Class
